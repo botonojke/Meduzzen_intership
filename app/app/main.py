1601bb7696c94981764ad0ffa89fefa67f6b66c4
boton <botonojke@icloud.com>
2022-10-13 00:14:29 +0300

    Add docker-compose with postgres and redis

--- app/app/main.py
+++ app/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import databases
 
 app = FastAPI()
+DATABASE_URL = "postgresql://meduzzen_user:pass@db:5432/meduzzen_db"
+
+database = databases.Database(DATABASE_URL)
+
 
 @app.get('/')
 async def status_root():
     return {"status": "Working"}
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
